b13d466b484b76854d2f04d51d5068094a6a7a55
Vadym Vlasenko <vadymvlasenko2007@gmail.com>
2026-05-31 13:44:18 +0300

    Add inline buttons

--- main.py
+++ main.py
@@ -34,6 +34,19 @@ def answer_callback(callback_query_id):
 
     requests.post(url, json=data)
 
+def edit_message(chat_id, message_id, text, reply_markup=None):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/editMessageText"
+
+    data = {
+        "chat_id": chat_id,
+        "message_id": message_id,
+        "text": text
+    }
+
+    if reply_markup is not None:
+        data["reply_markup"] = reply_markup
+
+    requests.post(url, json=data)
 
 def get_weather_statistic():
     url = (
@@ -152,6 +165,7 @@ while True:
 
             chat_id = callback["message"]["chat"]["id"]
             text = callback["data"]
+            message_id = callback["message"]["message_id"]
 
             if text == "weather":
                 text = "/weather"
